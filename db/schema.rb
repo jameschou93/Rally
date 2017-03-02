d82f95dc3f731e5cd5a20781509648d4760ce6a2
James Chou <jameschou1993@gmail.com>
2017-03-01 21:34:30 -0500

    trying to add a theme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226185047) do
+ActiveRecord::Schema.define(version: 20170302014436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170226185047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "bio"
+    t.boolean  "private?"
   end
 
   create_table "locations", force: :cascade do |t|
