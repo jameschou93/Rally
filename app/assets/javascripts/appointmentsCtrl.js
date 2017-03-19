b24969555c45e992687b15e51d66b99abac159b0
James Chou <jameschou1993@gmail.com>
2017-03-19 14:32:36 -0400

    working on filter

--- app/assets/javascripts/appointmentsCtrl.js
+++ app/assets/javascripts/appointmentsCtrl.js
@@ -14,11 +14,22 @@
       $http.get("/api/v1/appointments.json").then(function(response) {
         console.log(response.data);
         $scope.appointments = response.data;
-        var map = new google.maps.Map(document.getElementById('map'), {
+        var map;
+        map = new google.maps.Map(document.getElementById('map'), {
           zoom: 12,
           center: {lat: 40.7091841, lng: -74.0122789}
         });
+        $scope.appointments.forEach(function(appointment) {
+          var coords = { lat: appointment.latitude, lng: appointment.longitude };
+          var marker = new google.maps.Marker({
+            position: coords,
+            map: map
+          });
+        });
+
       });
     };
+
+    window.scope = $scope;
   });
 }());
