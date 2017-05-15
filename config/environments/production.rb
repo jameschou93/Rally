6fa4f17c1c02ca1c17722cf84aa8b6ed273e3da9
James Chou <jameschou1993@gmail.com>
2017-05-15 17:56:06 -0400

    added redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,10 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.web_socket_server_url = "wss://rally-.herokuapp.com/cable"
+  # config/environments/production.rb
+
+  config.action_cable.allowed_request_origins = ['https://rally-.herokuapp.com', 'http://rally-.herokuapp.com']
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
